6e5f01ce363f8cb7ac68ba01e2e9d136a7a2ad7c
Pipe97 <131320298+Pipe97@users.noreply.github.com>
2023-07-03 08:33:06 -0500

    Update axiosInstance.js
    Uptade api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://users-crud.academlo.tech/"
+    baseURL: "https://entregable-2-node.onrender.com/"
 });
 
 
